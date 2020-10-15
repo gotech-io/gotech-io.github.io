2b1960fb5d8efad0d9053a9af8b2e63ef77afc09
MohammadIdeas <mohammad.ideas@weissbeerger.com>
2020-10-15 17:52:55 +0300

    added animation to main page updated according to tasks

--- js/index.js
+++ js/index.js
@@ -10,58 +10,23 @@ function scrollFunction() {
   }
 }
 
+$(function() {
+  $('#main-title-container .header-container').animate({
+    top: '0px',
+    opacity : 1
+  }, 200);
 
-$('.navigation-links a').on("mouseover", function () {
-  $("#menu-item-text .letters").html(getWord($(this).html()));
-  var textWrapper = document.querySelector('.ml1 .letters');
-  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-  $(".letter").css('opacity', '0')
-  anime.timeline({loop: false})
-    .add({
-      targets: '.ml1 .letter',
-      scaleX: [0,1],
-      opacity: [0,1],
-      translateZ: [0,1],
-      easing: "easeOutExpo",
-      duration: 100,
-      delay: (el, i) => 50 * (i+1)
-    });
+  $('#header-nav a').animate({
+    top: '0',
+    opacity : 1
+  }, 100);
 
-  $('#menu-item-text').css('top', '0');
-}).on("mouseout", function () {
-  $('#menu-item-text').css('top', '-100%');
-}).on("click", function () {
-  $('#menu-item-text').css('top', '-100%');
+  $('.navigation-container .brand-logo').animate({
+    opacity : 1
+  }, 200);
+  $('#main-animation').animate({
+    opacity : 1
+  }, 300);
+
 });
 
-function getWord(word) {
-  return word;
-}
-// var textWrapper = document.querySelector('.ml1 .letters');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// anime.timeline({loop: true})
-//   .add({
-//     targets: '.ml1 .letter',
-//     scale: [0.3,1],
-//     opacity: [0,1],
-//     translateZ: 0,
-//     easing: "easeOutExpo",
-//     duration: 600,
-//     delay: (el, i) => 70 * (i+1)
-//   }).add({
-//     targets: '.ml1 .line',
-//     scaleX: [0,1],
-//     opacity: [0.5,1],
-//     easing: "easeOutExpo",
-//     duration: 700,
-//     offset: '-=875',
-//     delay: (el, i, l) => 80 * (l - i)
-//   }).add({
-//     targets: '.ml1',
-//     opacity: 0,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
-// particlesJS.load('particles-js', './particles.json')
