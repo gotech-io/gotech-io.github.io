71fb4a49bcf1467223f25212f9d3d22ef74a24a6
MohammadIdeas <mohammad.ideas@weissbeerger.com>
2020-10-15 15:52:17 +0300

    navigation + tasks changes

--- js/index.js
+++ js/index.js
@@ -12,7 +12,21 @@ function scrollFunction() {
 
 
 $('.navigation-links a').on("mouseover", function () {
-  $("#menu-item-text span").html(getWord($(this).html()));
+  $("#menu-item-text .letters").html(getWord($(this).html()));
+  var textWrapper = document.querySelector('.ml1 .letters');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  $(".letter").css('opacity', '0')
+  anime.timeline({loop: false})
+    .add({
+      targets: '.ml1 .letter',
+      scaleX: [0,1],
+      opacity: [0,1],
+      translateZ: [0,1],
+      easing: "easeOutExpo",
+      duration: 100,
+      delay: (el, i) => 50 * (i+1)
+    });
+
   $('#menu-item-text').css('top', '0');
 }).on("mouseout", function () {
   $('#menu-item-text').css('top', '-100%');
@@ -23,5 +37,31 @@ $('.navigation-links a').on("mouseover", function () {
 function getWord(word) {
   return word;
 }
+// var textWrapper = document.querySelector('.ml1 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-particlesJS.load('particles-js', './particles.json')
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml1 .letter',
+//     scale: [0.3,1],
+//     opacity: [0,1],
+//     translateZ: 0,
+//     easing: "easeOutExpo",
+//     duration: 600,
+//     delay: (el, i) => 70 * (i+1)
+//   }).add({
+//     targets: '.ml1 .line',
+//     scaleX: [0,1],
+//     opacity: [0.5,1],
+//     easing: "easeOutExpo",
+//     duration: 700,
+//     offset: '-=875',
+//     delay: (el, i, l) => 80 * (l - i)
+//   }).add({
+//     targets: '.ml1',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
+// particlesJS.load('particles-js', './particles.json')
