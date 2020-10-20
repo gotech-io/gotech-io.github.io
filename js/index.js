6e6af341606c21c46d46a3f7cf1983ae97dbca0d
MohammadIdeas <mohammad@testomate.io>
2020-10-20 11:24:14 +0300

    removed logo aimation

--- js/index.js
+++ js/index.js
@@ -43,58 +43,78 @@ $(function () {
 });
 
 
-(function () {
-  // Add event listener
-  document.addEventListener("mousemove", parallax);
-  // const elem = document.querySelector("#g-letter");
-  var g_letter = $('#g-letter')
-  // var g_letter = $('#g-letter')
-  // var g_letter = $('#g-letter')
-  g_letter.css('left', '10px')
-  // Magic happens here
-  function parallax(e) {
-    let _w = window.innerWidth / 2;
-    let _h = window.innerHeight / 2;
-    let _mouseX = e.clientX;
-    let _mouseY = e.clientY;
-    let _depth1 = `${(_mouseX - _w) * 0.01}%, ${(_mouseY - _h) * 0.01}%`;
-    let x = `translate( ${_depth1} )`;
-    // console.log(x);
-    // elem.style.transform = x;
-    g_letter.css('transform', x )
-  }
+// (function () {
+//   // Add event listener
+//   document.addEventListener("mousemove", parallax);
+//   // const elem = document.querySelector("#g-letter");
+//   // var g_letter = $('#g-letter')
+//   // var g_letter = $('#g-letter')
+//   g_letter.css('left', '10px')
+//   // Magic happens here
+//   function parallax(e) {
+//     let _w = window.innerWidth / 2;
+//     let _h = window.innerHeight / 2;
+//     let _mouseX = e.clientX;
+//     let _mouseY = e.clientY;
+//     let _depth1 = `${(_mouseX - _w) * 0.01}%, ${(_mouseY - _h) * 0.01}%`;
+//     let x = `translate( ${_depth1} )`;
+//     // console.log(x);
+//     // elem.style.transform = x;
+//     g_letter.css('transform', x)
+//   }
 
-})();
 
-// var object1=$('#green-blue-div');
-// var object2=$('#red-yellow-div');
-// var object3=$('#g-letter');
 
-// var layer=$('body');
+// })();
 
-// layer.mousemove(function(e){
-//    var valueX=(e.pageX * -1 / 12); 
-//    var valueY=(e.pageY * -1 / 12); 
-
-//     object1.css({
-//         'transform':'translate3d('+valueX+'px,'+valueY+'px,0) rotate(20deg)'
-//     });
+// $('body').mousemove(function (e) {
+//   var g_letter = $('#g-letter')
+//   let _w = window.innerWidth / 2;
+//   let _h = window.innerHeight / 2;
+//   let _mouseX = e.clientX;
+//   let _mouseY = e.clientY;
+//   let _depth1 = `${(_mouseX - _w) * 0.01}%, ${(_mouseY - _h) * 0.01}%`;
+//   let x = `translate( ${_depth1} )`;
+//   // console.log(x);
+//   // elem.style.transform = x;
+//   g_letter.css('transform', x)
 // });
 
-// layer.mousemove(function(e){
-//    var valueX=(e.pageX * -1 / 15); 
-//    var valueY=(e.pageY * -1 / 20); 
+// $(document).ready(function () {
+//   var $one = $('#g-letter');
+//   var $two = $('#red-yellow-div');
+//   var $three = $('#green-blue-div'),
+//     browserPrefix = "",
+//     usrAg = navigator.userAgent;
+//   if (usrAg.indexOf("Chrome") > -1 || usrAg.indexOf("Safari") > -1) {
+//     browserPrefix = "-webkit-";
+//   } else if (usrAg.indexOf("Opera") > -1) {
+//     browserPrefix = "-o";
+//   } else if (usrAg.indexOf("Firefox") > -1) {
+//     browserPrefix = "-moz-";
+//   } else if (usrAg.indexOf("MSIE") > -1) {
+//     browserPrefix = "-ms-";
+//   }
 
-//     object2.css({
-//         'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
-//     });
-// });
+//   $(document).mousemove(function (event) {
+//     var cx = Math.ceil(window.innerWidth / 2.0),
+//       cy = Math.ceil(window.innerHeight / 2.0),
+//       dx = event.pageX - cx,
+//       dy = event.pageY - cy,
+//       tiltx = (dy / cy),
+//       tilty = - (dx / cx),
+//       radius = Math.sqrt(Math.pow(tiltx, 2) + Math.pow(tilty, 2)),
+//       degree = (radius * 15);
 
-// layer.mousemove(function(e){
-//    var valueX=(e.pageX * -1 / 15); 
-//    var valueY=(e.pageY * -1 / 15); 
+//     shadx = degree * tiltx;   /*horizontal shadow*/
+//     shady = degree * tilty;   /*vertical shadow*/
 
-//     object3.css({
-//         'transform':'translate3d('+valueX+'px,'+valueY+'px,0) rotate(-20deg)'
-//     });
+//     $one.css(browserPrefix + 'transform', 'rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+//     $two.css(browserPrefix + 'transform', 'rotate3d(' + tiltx + ', ' + -tilty + ', 0, ' + degree + 'deg)');
+//     $three.css(browserPrefix + 'transform', 'rotate3d(' + -tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+
+//     /*       if(dx>cx) without that horizontal values are reversed
+//               $('#div1').css('box-shadow', + (-shady) + 'px ' + (-shadx) +'px 5px #3D352A');
+//           else $('#div1').css('box-shadow', + shady + 'px ' + (-shadx) +'px 5px #3D352A'); */
+//   });
 // });
