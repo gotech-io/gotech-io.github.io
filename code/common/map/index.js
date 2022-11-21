6045264727386cd5fc5137ffb410bd55394f0957
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-11-21 12:43:39 +0200

    map test

---
+++ code/common/map/index.js
@@ -0,0 +1,23 @@
+import GoogleMapReact from "google-map-react";
+import React from "react";
+
+function Map({city, street, house}) {
+
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
+  );
+}
+
+export default Map;
---
+++ content/home/md/contact/map/index.md
@@ -0,0 +1,8 @@
+---
+layout: common/map/index
+title: contact-map
+city: Netanya
+street: Ha-Melakha
+
+---
+<!-- house: '5' -->
