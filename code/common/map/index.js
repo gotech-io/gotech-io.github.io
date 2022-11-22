01fe62758b839ddb8d309eeb07d3bc2a2091d051
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-11-22 11:45:16 +0200

    styles changed

--- code/common/map/index.js
+++ code/common/map/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 
-const GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_SECRET_KEY
-
 function Map({city, street, house}) {
 const src = `https://www.google.com/maps?q=${house} ${street}, ${city}&output=embed`
-//const src = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_MAP_KEY}&q=${house} ${street}, ${city}&output=embed`
   return (
     <iframe className="map"
       loading="lazy"
