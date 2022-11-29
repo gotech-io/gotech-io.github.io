96c039884b7fcfd3449539e0e34e2bc2474af3de
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-11-29 15:10:55 +0200

    footer

--- code/common/footer/index.js
+++ code/common/footer/index.js
@@ -7,7 +7,6 @@ import { handleUrl } from "../../utils";
 
 const Footer = ({
   variables,
-  socials,
   _relativeURL,
   _ID,
   copyright,
@@ -17,21 +16,9 @@ const Footer = ({
   terms_of_use_url,
 }) => {
   return (
-    // <footer className="footer">
-    //   <div className="footer-content">
     //     <a href="/#" className="footer-logo">
     //       <Image src={handleUrl(images.common.logo, _relativeURL, _ID)} />
     //     </a>
-    //     {socials}
-    //     <div className="footer-bottom">
-    //       <p>{copyright}</p>
-    //       <section className="flex-start footer-bottom-policy">
-    //         <a href={privacy_policy_url}>{privacy_policy_text}</a>
-    //         <a href={terms_of_use_url}>{terms_of_use_text}</a>
-    //       </section>
-    //     </div>
-    //   </div>
-    // </footer>
 
     <div className="footer">
       <div className="block">
--- content/_shared/footer/index.md
+++ content/_shared/footer/index.md
@@ -32,8 +32,6 @@ variables:
     },
   ]
 
-socials:
-  - /_shared/socials/index.md
 copyright: © Copyright GoTech. All rights reserved.
 privacy_policy_text: Privacy Policy
 privacy_policy_url: /privacy-policy
