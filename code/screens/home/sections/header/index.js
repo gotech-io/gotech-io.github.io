e5f481923d76549a06efef139deafef98ec0d654
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-11-29 15:16:49 +0200

    main

--- code/screens/home/sections/header/index.js
+++ code/screens/home/sections/header/index.js
@@ -1,6 +1,6 @@
+import Image from "../../../../common/img";
 import React from "react";
 import animations from "../../../../../assets/js/animations";
-import Image from "../../../../common/img";
 import { handleUrl } from "../../../../utils";
 
 const HomeHeader = ({
@@ -15,12 +15,37 @@ const HomeHeader = ({
 }) => {
   return (
     <div className="home-header">
-      <Image
-        src={handleUrl(background, _relativeURL, _ID)}
-        customClassName="home-header-background"
-      />
+      <div className="home-header-background">
+        <div className="orange-blobs">
+          <lottie-player
+            src="https://assets3.lottiefiles.com/packages/lf20_5gdcuanx.json"
+            background="transparent"
+            style={{ width: "1300px", height: "1300px" }}
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
 
-      <div className="home-header-main flex-between">
+        <div className="green-blobs">
+          <lottie-player
+            src="https://lottie.host/2a4cd7fd-ef68-4865-b56c-8fe21f2c9170/7RneaIKFUy.json"
+            background="transparent"
+            style={{
+              width: "1300px",
+              height: "1300px",
+              position: "absolute",
+              left: "-550px",
+              bottom: "-500px",
+            }}
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+      </div>
+
+      <div className="home-header-main flex-center">
         <section className="home-header-main-content">
           <h1 data-aos={animations.up} data-aos-delay="100">
             {title}
@@ -28,12 +53,6 @@ const HomeHeader = ({
           <h2 data-aos={animations.up} data-aos-delay="250">
             {sub_title}
           </h2>
-          <div data-aos={animations.up} data-aos-delay="400">
-            {_body}
-          </div>
-          <span data-aos={animations.zoomIn} data-aos-delay="800">
-            {button}
-          </span>
         </section>
       </div>
       <a href="/#services">{toggle_animation}</a>
