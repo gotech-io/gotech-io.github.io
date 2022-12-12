32595831e245f8334b915aca0c7540d6de6cb416
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-12 14:26:49 +0200

    refactor

--- code/screens/home/sections/header/components/blobs/index.js
+++ code/screens/home/sections/header/components/blobs/index.js
@@ -1,29 +1,36 @@
 import React from "react";
 
-const Blobs = ({ orange, green }) => {
+const blobs = [
+  {
+    className: "orange-blobs",
+    src: "https://assets3.lottiefiles.com/packages/lf20_5gdcuanx.json",
+    size: 1300,
+  },
+  {
+    className: "green-blobs",
+    src: "https://lottie.host/2a4cd7fd-ef68-4865-b56c-8fe21f2c9170/7RneaIKFUy.json",
+    size: 1100,
+  },
+];
+
+const Blobs = () => {
   return (
     <div className="home-header-background">
-      <div className="orange-blobs">
-        <lottie-player
-          src="https://assets3.lottiefiles.com/packages/lf20_5gdcuanx.json"
-          background="transparent"
-          style={{ width: `${orange}px`, height: `${orange}px` }}
-          speed="1"
-          loop
-          autoplay
-        ></lottie-player>
-      </div>
-
-      <div className="green-blobs">
-        <lottie-player
-          src="https://lottie.host/2a4cd7fd-ef68-4865-b56c-8fe21f2c9170/7RneaIKFUy.json"
-          background="transparent"
-          style={{ width: `${green}px`, height: `${green}px` }}
-          speed="1"
-          loop
-          autoplay
-        ></lottie-player>
-      </div>
+      {blobs.map((blob) => {
+        const { className, src, size } = blob;
+        return (
+          <div key={className} className={className}>
+            <lottie-player
+              src={src}
+              background="transparent"
+              style={{ width: `${size}px`, height: `${size}px` }}
+              speed="1"
+              loop
+              autoplay
+            ></lottie-player>
+          </div>
+        );
+      })}
     </div>
   );
 };
