2f76ffcab2ff15ae3d7a573801a15d7120ffc23b
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-12 13:06:49 +0200

    style

--- code/screens/home/sections/header/index.js
+++ code/screens/home/sections/header/index.js
@@ -53,29 +53,3 @@ const HomeHeader = ({ title, sub_title, description, button }) => {
 };
 
 export default HomeHeader;
-
-//return (
-// <div className="home-header">
-//   <Image
-//     src={handleUrl(background, _relativeURL, _ID)}
-//     customClassName="home-header-background"
-//   />
-//   <div className="home-header-main flex-between">
-//     <section className="home-header-main-content">
-//       <h1 data-aos={animations.up} data-aos-delay="100">
-//         {title}
-//       </h1>
-//       <h2 data-aos={animations.up} data-aos-delay="250">
-//         {sub_title}
-//       </h2>
-//       <div data-aos={animations.up} data-aos-delay="400">
-//         {_body}
-//       </div>
-//       <span data-aos={animations.zoomIn} data-aos-delay="800">
-//         {button}
-//       </span>
-//     </section>
-//   </div>
-//   <a href="/#services">{toggle_animation}</a>
-// </div>
-//);
