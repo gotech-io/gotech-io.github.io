c9acdc0f60bc0643673365b4c855e9c4ea594770
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-13 18:06:20 +0200

    refactor

---
+++ assets/js/common/case-studies-projects/memomi.js
@@ -0,0 +1,49 @@
+export const MEMOMI = {
+  title: "Memomi ",
+  description:
+    "Memomi is the award-winning smart mirror software transforming the luxury shopping market.",
+  images: {
+    mockup: {
+      src: "/assets/images/case-studies/memomi/memomi1.png",
+      alt: "memomi",
+    },
+    main: {
+      src: "/assets/images/case-studies/memomi/memomi2.png",
+      alt: "memomi",
+    },
+    parallax: {
+      src: "/assets/images/case-studies/memomi/memomi3.jpg",
+      alt: "memomi",
+    },
+  },
+  client: {
+    title: "About Client:",
+    description:
+      "Memomi is a developer of an augmented reality and artificial intelligence platform that enables customers to try products virtually and receive personalized product recommendations. They approached GoTech needing help integrating an AR engine between their server and mobile applications.",
+  },
+  challenge: {
+    title: "Challenge Faced:",
+    description:
+      "Memomi needed assistance integrating their complex AR engine between the server and mobile applications.",
+    steps: [
+      "Needed to be integrating the SDK into their clients' applications",
+      "It was necessary to develop a system for machine learning and augmented reality",
+    ],
+  },
+  solution: {
+    title: "Our Solution:",
+    description:
+      "GoTech was able to help Memomi integrate the AR engine into their server and mobile applications, both for iOS and Android. Our team of experts has experience in AR and ML, and was able to deliver high-quality mobile applications that offered a seamless and engaging AR experience. We also assisted Memomi in integrating the SDK into their clients' applications, further enhancing the functionality of their platform. With GoTech's help, Memomi was able to overcome the challenges of integrating their AR engine and take their platform to the next level. Memomi was eventually acquired by Walmart.",
+  },
+  introduce: [
+    { title: "Country", description: "Israel" },
+    { title: "Industry", description: "AR Market" },
+    { title: "Services Used", options: ["Mobile"], link: "/#services" },
+    {
+      title: "Technologies Stack",
+      options: ["iOS", "Android", "ML", "AR", "SDK"],
+    },
+  ],
+  tags: ["iOS", "Android", "ML", "AR", "SDK"],
+  button: { url: "/case-studies-project/memomi", btnText: "see more" },
+};
---
+++ assets/js/common/case-studies-projects/orbs.js
@@ -0,0 +1,48 @@
+export const ORBS = {
+  title: "Orbs ",
+  description:
+    "Orbs is an open, decentralized and public blockchain infrastructure executed by a secure network of permissionless validators using Proof-of-Stake (PoS) consensus.",
+  images: {
+    mockup: {
+      src: "/assets/images/case-studies/orbs/orbs1.png",
+      alt: "orbs",
+    },
+    main: {
+      src: "/assets/images/case-studies/orbs/orbs2.png",
+      alt: "orbs",
+    },
+    parallax: {
+      src: "/assets/images/case-studies/orbs/orbs3.jpg",
+      alt: "orbs",
+    },
+  },
+  client: {
+    title: "About Client:",
+    description:
+      "Orbs is a developer of a public blockchain infrastructure designed for mass usage applications and close integration with EVM-based L1’s such as Ethereum, Binance SmartChain and Polygon. They came to GoTech needing help building complex Front-End projects, including dashboards, monitors, and the company's website.",
+  },
+  challenge: {
+    title: "Challenge Faced:",
+    description:
+      "Orbs was looking for a partner who could take ownership of entire projects, think big, and work independently while providing high-quality products. ",
+    steps: [
+      "They needed a team that could handle the complexity of their Front-End projects",
+    ],
+  },
+  solution: {
+    title: "Our Solution:",
+    description:
+      "GoTech was able to help Memomi integrate the AR engine into their server and mobile applications, both for iOS and Android. Our team of experts has experience in AR and ML, and was able to deliver high-quality mobile applications that offered a seamless and engaging AR experience. We also assisted Memomi in integrating the SDK into their clients' applications, further enhancing the functionality of their platform. With GoTech's help, Memomi was able to overcome the challenges of integrating their AR engine and take their platform to the next level. Memomi was eventually acquired by Walmart.",
+  },
+  introduce: [
+    { title: "Country", description: "Israel" },
+    { title: "Industry", description: "Blockchain Infrastructure" },
+    { title: "Services Used", options: ["FrontEnd"], link: "/#services" },
+    {
+      title: "Technologies Stack",
+      options: ["React", "Cuttlebelle", "Redux"],
+    },
+  ],
+  tags: ["React", "Cuttlebelle", "Redux"],
+  button: { url: "/case-studies-project/orbs", btnText: "see more" },
+};
