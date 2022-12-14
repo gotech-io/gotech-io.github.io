701f8e92e7bf62fac1fb616af0c67c4e776bcfc3
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-14 18:39:16 +0200

    marquee style

--- code/common/marquee/index.js
+++ code/common/marquee/index.js
@@ -2,22 +2,13 @@ import React from "react";
 
 function Marquee({ marquee }) {
   return (
-    <div className="marquee-container">
+    <div className="marquee-wrapper">
       <div className="marquee">
         {marquee.map((src, i) => {
           return (
-            <span key={i} className="marquee-item">
+            <div key={i} className="marquee-item">
               <img src={src} alt="client images" />
-            </span>
-          );
-        })}
-      </div>
-      <div className="marquee delay ">
-        {marquee.map((src, i) => {
-          return (
-            <span key={i} className="marquee-item">
-              <img src={src} alt="client images" />
-            </span>
+            </div>
           );
         })}
       </div>
