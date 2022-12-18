e5ad3140b30d6026841ce35d1d88b3fb5330e4d1
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-18 09:47:32 +0200

    life cycle

---
+++ code/common/life-cycle/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import animations from "../../../assets/js/animations";
+
+const stages = [
+  { title: "Strategy", icon: "fa-solid fa-pencil" },
+  { title: "Design", icon: "fa-solid fa-wand-magic-sparkles" },
+  { title: "Develop", icon: "fa-solid fa-user-gear" },
+  { title: "Testing", icon: "fa-solid fa-bug" },
+  { title: "Launch", icon: "fa-solid fa-handshake" },
+  { title: "Support", icon: "fa-solid fa-headset" },
+];
+
+const LifeCycle = () => {
+  return (
+    <div className="stages">
+      <div className="stages-item-line" />
+      <div className="page-common-block stages-wrapper">
+        {stages.length &&
+          stages.map((stage) => {
+            const { title, icon } = stage;
+            return (
+              <div key={performance.now()} className="stages-item">
+                <div
+                  className="stages-item-wrapper"
+                  data-aos={animations.zoomIn}
+                >
+                  <div className="stages-item-wrapper-dot">
+                    <i className={icon}></i>
+                  </div>
+                </div>
+                <p>{title}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default LifeCycle;
---
+++ code/screens/services/service-page/index.js
@@ -0,0 +1,66 @@
+import Header from "../../../common/header";
+import LifeCycle from "../../../common/life-cycle";
+import React from "react";
+import Technologies from "../../../common/technologies";
+import { SERVICES } from "../../../../assets/js/common/content/services";
+import { handleUrl } from "../../../utils";
+
+function ServicePage({ entity, _relativeURL, _ID }) {
+  const { title, description, services } = SERVICES[entity];
+  return (
+    <div>
+      {/* <Header title={title} description={description} />
+
+      <LifeCycle />
+
+      <div className="services-technology">
+        <div className="page-common-block">
+          <div className="services-technology-wrapper">
+            {services.map((service) => {
+              return (
+                <div
+                  key={performance.now()}
+                  className="services-technology-block"
+                >
+                  <div className="services-technology-block-wrapper">
+                    <div className="services-technology-button">
+                      <i className="fa-solid fa-arrow-down" />
+                    </div>
+                  </div>
+
+                  <div className="accordion-container">
+                    <div className="accordion-label">
+                      <h4>{service.title}</h4>
+                      <p>{service.description}</p>
+                    </div>
+                    <div className="accordion-content">
+                      <Technologies technologies={service.technologies} />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+      <div className="services-block">
+        <div className="page-common-block">
+          <p className="services-block-description">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </div>
+      </div> */}
+
+      <script
+        type="module"
+        src={handleUrl(`/assets/js/screens/about/index.js`, _relativeURL, _ID)}
+      />
+    </div>
+  );
+}
+
+export default ServicePage;
