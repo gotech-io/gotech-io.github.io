de75a02086fc950bf06bd63d659ea8078de058de
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-18 10:12:36 +0200

    added content

--- code/screens/services/index.js
+++ code/screens/services/index.js
@@ -1,13 +1,54 @@
 import React from "react";
+import animations from "../../../assets/js/animations";
 import { handleUrl } from "../../utils";
 
-function Services({ _relativeURL, _ID }) {
+//import Diagram from "../../common/diagram";
+//import Header from "../../common/header";
+//import ListServices from "./components/list";
+//import Points from "../../common/points";
+
+function Services({ title, description, info, _relativeURL, _ID }) {
+  const { services, intro, diagram } = info;
+  const { main, second, third } = description;
   return (
-    <div className="case-studies">
-      Services
+    <div>
+      {/* <Header title={title} description={main} /> */}
+      Header
+      <div className="services-block">
+        <div className="page-common-block">
+          <p className="services-block-description">{second}</p>
+        </div>
+      </div>
+      <div className="page-common-block">
+        ListServices
+        {/* <ListServices services={services} /> */}
+      </div>
+      <div className="services-block">
+        <div className="page-common-block">
+          <p className="services-block-description">{third}</p>
+        </div>
+      </div>
+      <div className="services-intro">
+        <div className="page-common-block">
+          <h4>{intro.title}</h4>
+          <p>{intro.description}</p>
+        </div>
+        Points
+        {/* <Points points={intro.points} /> */}
+      </div>
+      <div className="services-diagram">
+        <div className="page-common-block">
+          <h4>{diagram.title}</h4>
+          <p>{diagram.description}</p>
+          <div className="services-diagram-wrapper">
+            Diagram
+            {/* <Diagram /> */}
+          </div>
+        </div>
+      </div>
       <script
         type="module"
-        src={handleUrl(`/assets/js/about/index.js`, _relativeURL, _ID)}
+        src={handleUrl(`/assets/js/screens/about/index.js`, _relativeURL, _ID)}
       />
     </div>
   );
