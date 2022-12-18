308e3407290ac36ab86ff1477ac029b86bb4771a
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-18 12:31:24 +0200

    added btn

--- code/screens/services/components/item/index.js
+++ code/screens/services/components/item/index.js
@@ -1,20 +1,20 @@
-import Link from "../../../../common/btns/link";
+import ArrowButton from "../../../../common/btns/arrow";
 import React from "react";
 import { ICONS } from "../../../../../assets/js/common/content/icons";
 import { SERVICES } from "../../../../../assets/js/common/content/services";
 
-const btnText = "read more";
+const textBtn = "read more";
 
 const ItemServices = ({ entity }) => {
   const { title, details, url } = SERVICES[entity];
   return (
     <div className="services-content-item-info">
       <i className={ICONS[entity]} />
-
       <h6>{title}</h6>
       <p>{details}</p>
+
       <div className="services-content-item-info-btn">
-        <Link url={url} btnText={btnText} />
+        <ArrowButton link={url} textBtn={textBtn} />
       </div>
     </div>
   );
