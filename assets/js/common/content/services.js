3a462935dc5d64a5ec3d538f7f7992a34e11a04a
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-18 11:12:23 +0200

    service

---
+++ assets/js/common/content/services.js
@@ -0,0 +1,172 @@
+export const SERVICES = {
+  WEB_DEV: {
+    title: "Web Development",
+    details:
+      "Web teams to be responsible for the coding, innovative design and layout of the website",
+    description:
+      "Web teams to be responsible for the coding, innovative design and layout of the website. Web developers responsibilities include building the website from concept all the way to completion.",
+    url: "/service/web",
+    services: [
+      {
+        title: "Frontend",
+        description:
+          "The highly skilled GoTech teams are experts at developing applications. Utilizing the most innovative technologies, we develop web applications for organizations and entrepreneurs from a large range of fields. By creating coherent interfaces, we ensure a positive and empowering user experience.",
+        technologies: [
+          { key: "Languages", icons: [] },
+          {
+            key: "Frameworks",
+            icons: [
+              "/assets/images/technologies/angular.png",
+              "/assets/images/technologies/react.png",
+            ],
+          },
+          {
+            key: "Libraries",
+            icons: [
+              "/assets/images/technologies/redux.png",
+              "/assets/images/technologies/mobx.png",
+              "/assets/images/technologies/zustand.png",
+            ],
+          },
+        ],
+      },
+      {
+        title: "Backend",
+        description:
+          "GoTech implement the best server-side development methodologies. Developing interfaces and applications using Python / Node.js programming languages, which are considered the most advanced tools for backend development. ",
+        technologies: [
+          {
+            key: "Languages",
+            icons: [
+              "/assets/images/technologies/python.jpeg",
+              "/assets/images/technologies/java.png",
+              "/assets/images/technologies/kotlin.png",
+              "/assets/images/technologies/c.png",
+            ],
+          },
+          {
+            key: "Frameworks",
+            icons: [
+              "/assets/images/technologies/django.png",
+              "/assets/images/technologies/node.png",
+            ],
+          },
+          { key: "Libraries", icons: [] },
+        ],
+      },
+    ],
+  },
+  MOBILE_DEV: {
+    title: "Mobile Development",
+    details: "Mobile app development allows your business to be at hand always",
+    description:
+      "Mobile app development allows your business to be at hand always",
+    url: "/service/mobile",
+    services: [
+      {
+        title: "iOS",
+        description:
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        technologies: [
+          {
+            key: "Languages",
+            icons: ["/assets/images/technologies/swift.png"],
+          },
+          { key: "Frameworks", icons: [] },
+          { key: "Libraries", icons: [] },
+        ],
+      },
+      {
+        title: "Android",
+        description:
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        technologies: [
+          {
+            key: "Languages",
+            icons: [
+              "/assets/images/technologies/python.jpeg",
+              "/assets/images/technologies/java.png",
+              "/assets/images/technologies/kotlin.png",
+            ],
+          },
+          {
+            key: "Frameworks",
+            icons: ["/assets/images/technologies/django.png"],
+          },
+          { key: "Libraries", icons: [] },
+        ],
+      },
+    ],
+  },
+  DEVOPS_DEV: {
+    title: "DevOps",
+    details: "Choose smart development and upload your business to cloud",
+    description:
+      "Choose smart development and upload your business to cloud. Choose smart development and upload your business to cloud.",
+    url: "/service/devops",
+    services: [
+      {
+        title: "DevOps",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        technologies: [
+          {
+            key: "Languages",
+            icons: [
+              "/assets/images/technologies/python.jpeg",
+              "/assets/images/technologies/java.png",
+              "/assets/images/technologies/kotlin.png",
+            ],
+          },
+          {
+            key: "Frameworks",
+            icons: [
+              "/assets/images/technologies/django.png",
+              "/assets/images/technologies/node.png",
+            ],
+          },
+          {
+            key: "Libraries",
+            icons: [
+              "/assets/images/technologies/docker.png",
+              "/assets/images/technologies/kubernetes.png",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  QA_DEV: {
+    title: "QA Automation",
+    details:
+      "We are interested in high quality of our products and we care about our reputation",
+    description:
+      "We are interested in high quality of our products and we care about our reputation",
+    url: "/service/qa",
+    services: [
+      {
+        title: "QA Automation",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        technologies: [
+          {
+            key: "Languages",
+            icons: [
+              "/assets/images/technologies/python.jpeg",
+              "/assets/images/technologies/java.png",
+              "/assets/images/technologies/kotlin.png",
+            ],
+          },
+          {
+            key: "Frameworks",
+            icons: [
+              "/assets/images/technologies/django.png",
+              "/assets/images/technologies/node.png",
+            ],
+          },
+          { key: "Libraries", icons: [] },
+        ],
+      },
+    ],
+  },
+};
