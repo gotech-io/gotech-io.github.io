8bdea5264537692c406f7f07ce122fc25d4c643c
Alina Khomenker <alinakhomenker@Alinas-MacBook-Pro.local>
2022-12-19 12:39:49 +0200

    singe service

--- code/screens/services/service-page/index.js
+++ code/screens/services/service-page/index.js
@@ -1,10 +1,9 @@
 import Header from "../../../common/header";
 import React from "react";
 import StagesCycle from "../../../common/stages-cycle";
+import Technologies from "../../../common/technologies";
 import { SERVICES } from "../../../../assets/js/common/content/services";
 import { handleUrl } from "../../../utils";
-
-//import Technologies from "../../../common/technologies";
 
 function ServicePage({ entity, _relativeURL, _ID }) {
   const { title, description, services } = SERVICES[entity];
@@ -29,31 +28,35 @@ function ServicePage({ entity, _relativeURL, _ID }) {
 
       <StagesCycle />
 
-      {/* <div className="services-technology">
+      <div className="services-block">
         <div className="page-common-block">
+          <p className="services-block-description">
+            "Our QA Automation services provide complete solutions for
+            automating your software testing processes. We offer services such
+            as implementing and integrating automation infrastructure,
+            developing test tools and reporting tools, and providing automation
+            training to your team. Our experts have extensive experience in
+            automation, and can help you create stable and maintainable test
+            automation systems that will save time and improve the reliability
+            of your software."
+          </p>
+        </div>
+      </div>
+
+      <div className="services-technology">
+        <div className="page-common-block">
+          <div>
+            <h4>Title</h4>
+            <p>gfcvhgjhkjlk;lkghfdgh gjhgjhgkjhkhkjg</p>
+          </div>
+
           <div className="services-technology-wrapper">
             {services.map((service) => {
               return (
-                <div
+                <Technologies
                   key={performance.now()}
-                  className="services-technology-block"
-                >
-                  <div className="services-technology-block-wrapper">
-                    <div className="services-technology-button">
-                      <i className="fa-solid fa-arrow-down" />
-                    </div>
-                  </div>
-
-                  <div className="accordion-container">
-                    <div className="accordion-label">
-                      <h4>{service.title}</h4>
-                      <p>{service.description}</p>
-                    </div>
-                    <div className="accordion-content">
-                      <Technologies technologies={service.technologies} />
-                    </div>
-                  </div>
-                </div>
+                  technologies={service.technologies}
+                />
               );
             })}
           </div>
@@ -63,13 +66,17 @@ function ServicePage({ entity, _relativeURL, _ID }) {
       <div className="services-block">
         <div className="page-common-block">
           <p className="services-block-description">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum.
+            "Our QA Automation services provide complete solutions for
+            automating your software testing processes. We offer services such
+            as implementing and integrating automation infrastructure,
+            developing test tools and reporting tools, and providing automation
+            training to your team. Our experts have extensive experience in
+            automation, and can help you create stable and maintainable test
+            automation systems that will save time and improve the reliability
+            of your software."
           </p>
         </div>
-      </div> */}
+      </div>
 
       <script
         type="module"
