5cffcc25037be4fb43ca41340fe87dcea2337a98
Alina Khomenker <alina.khomenker@gotech.io>
2022-12-19 16:31:39 +0200

    refactor

--- code/screens/services/index.js
+++ code/screens/services/index.js
@@ -11,48 +11,41 @@ import { COLORS } from "../../../assets/js/common/colors";
 import { PAGE_DETAILS } from "../../../assets/js/common/services/page-details";
 import { handleUrl } from "../../utils";
 
-function Services({ marquee, contact, _relativeURL, _ID }) {
-  const { title, description, info } = PAGE_DETAILS;
-  //const { services, intro, diagram } = info;
-  const { main, second, third } = description;
+function Services({ services, marquee, contact, _relativeURL, _ID }) {
+  const { title, description, sections, intro, diagram } = PAGE_DETAILS;
   return (
-    // <div>
-    //   <Header title={title} description={main} />
-
-    //   <div className="services-block">
-    //     <div className="page-common-block">
-    //       <p className="services-block-description">{second}</p>
-    //     </div>
-    //   </div>
-
-    //   <div className="page-common-block">
-    //     <ListServices services={services} />
-    //   </div>
-
-    //   <div className="services-block">
-    //     <div className="page-common-block">
-    //       <p className="services-block-description">{third}</p>
-    //     </div>
-    //   </div>
-
-    //   <PointsSection intro={intro} />
-
-    //   <DiagramSection diagram={diagram} />
-
-    //   <script
-    //     type="module"
-    //     src={handleUrl(`/assets/js/screens/about/index.js`, _relativeURL, _ID)}
-    //   />
-    // </div>
     <PageWrapper
       title={title}
-      description={main}
+      description={description}
       marquee={marquee}
       contact={contact}
       _relativeURL={_relativeURL}
       _ID={_ID}
     >
-      <ColorSection color={COLORS.GREEN} title={second} />
+      {sections.length &&
+        sections.map(({ type, paragraphs }) => {
+          return (
+            <div key={type}>
+              <ColorSection
+                color={COLORS.GREEN}
+                paragraphs={paragraphs}
+              ></ColorSection>
+
+              {type === "section-first" && (
+                <div className="page-common-block">
+                  <ListServices services={services} />
+                </div>
+              )}
+
+              {type === "section-second" && (
+                <div>
+                  <PointsSection intro={intro} />
+                  <DiagramSection diagram={diagram} />
+                </div>
+              )}
+            </div>
+          );
+        })}
     </PageWrapper>
   );
 }
--- content/services/md/index.md
+++ content/services/md/index.md
@@ -1,5 +1,11 @@
 ---
 layout: screens/services/index
+services:
+  - content/web.md
+  - content/mobile.md
+  - content/devops.md
+  - content/qa.md
+
 marquee:
   - /_shared/marquee/index.md
 contact:
