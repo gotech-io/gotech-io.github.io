c7728af95e1f2e51ff07916d1f9dd4cec756e0bd
Alina Khomenker <alina.khomenker@gotech.io>
2022-12-26 16:57:25 +0200

    style

--- code/common/steps/index.js
+++ code/common/steps/index.js
@@ -3,11 +3,13 @@ import Step from "../step";
 
 const Steps = ({ steps }) => {
   return (
-    <div className="steps">
-      {steps.length &&
-        steps.map((step, i) => {
-          return <Step key={step} step={step} number={i + 1} />;
-        })}
+    <div className="page-common-block">
+      <div className="steps">
+        {steps.length &&
+          steps.map((step, i) => {
+            return <Step key={step} step={step} number={i + 1} />;
+          })}
+      </div>
     </div>
   );
 };
