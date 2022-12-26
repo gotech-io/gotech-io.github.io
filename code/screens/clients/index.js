d7784450f48622dbbd761f0122e7195c0052bd13
Alina Khomenker <alina.khomenker@gotech.io>
2022-12-26 11:44:02 +0200

    mobile styles

--- code/screens/clients/index.js
+++ code/screens/clients/index.js
@@ -1,16 +1,9 @@
 import Categories from "../../common/categories";
-import Category from "./components/category";
 import ColorSection from "../../common/ColorSection";
-import Flags from "../../common/flags";
-import Left from "./components/left";
 import PageWrapper from "../../common/pageWrapper";
 import React from "react";
-import StagesCycle from "../../common/stages-cycle";
-import TitleSection from "../../common/titleSection";
 import { CLIENT_DETAILS } from "../../../assets/js/common/clients";
-import { CLIENT_TITLES } from "../../../assets/js/common/clients/const";
 import { listCategories } from "../../../assets/js/common/clients/listCategories";
-import { COLORS } from "../../../assets/js/common/colors";
 
 function Clients({ marquee, contact, _relativeURL, _ID }) {
   const { title, description, sections } = CLIENT_DETAILS;
