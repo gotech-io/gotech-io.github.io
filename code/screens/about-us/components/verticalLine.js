fe11254dcd8ee6277b5d0763bd32d856732c80f1
Alina Khomenker <alina.khomenker@gotech.io>
2022-12-27 14:38:11 +0200

    timeline refactor

---
+++ code/screens/about-us/components/verticalLine.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VerticalLine = () => {
+  return (
+    <div className="timeline-vertical-block">
+      <hr className="timeline-vertical-line" />
+      <i className="fa-solid fa-arrow-down"></i>
+    </div>
+  );
+};
+
+export default VerticalLine;
--- code/screens/about-us/index.js
+++ code/screens/about-us/index.js
@@ -1,8 +1,6 @@
-import ColorSection from "../../common/ColorSection";
 import PageWrapper from "../../common/pageWrapper";
 import React from "react";
-import TimelineItem from "./components/timelineItem";
-import { TIMELINE } from "../../../assets/js/common/timeline";
+import Timeline from "./components/timeline";
 
 function AboutUs({ title, description, marquee, contact, _relativeURL, _ID }) {
   return (
@@ -14,31 +12,7 @@ function AboutUs({ title, description, marquee, contact, _relativeURL, _ID }) {
       _relativeURL={_relativeURL}
       _ID={_ID}
     >
-      <div className="page-common-block">
-        <div className="timeline-wrapper">
-          <div className="timeline-vertical-block">
-            <hr className="timeline-vertical-line" />
-            <i className="fa-solid fa-arrow-down"></i>
-          </div>
-
-          <div className="timeline-block">
-            {TIMELINE.length &&
-              TIMELINE.map(({ title, description, year, color }, i) => {
-                return (
-                  <TimelineItem
-                    key={title}
-                    title={title}
-                    description={description}
-                    number={i + 1}
-                    year={year}
-                    color={color}
-                    reverse={i % 2 === 0 ? "reverse" : ""}
-                  />
-                );
-              })}
-          </div>
-        </div>
-      </div>
+      <Timeline />
     </PageWrapper>
   );
 }
