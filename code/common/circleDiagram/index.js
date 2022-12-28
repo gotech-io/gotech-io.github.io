7a4e305560e4acba279d8deb6dd2bc67d2855c12
Alina Khomenker <alina.khomenker@gotech.io>
2022-12-28 18:39:03 +0200

    circle diagram

--- code/common/circleDiagram/index.js
+++ code/common/circleDiagram/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+function CircleDiagramRedItem() {
+  return (
+    <div className="circle-diagram-red-services-wrapper">
+      <div className="circle-diagram-red-services-icons"></div>
+    </div>
+  );
+}
+
 function CircleDiagramInnerItem() {
   return (
     <div className="circle-diagram-services-wrapper">
@@ -13,21 +21,28 @@ function CircleDiagram() {
     <div className="page-common-block">
       <div className="circle-diagram">
         <div className="circle-diagram-line-inner">
+          <div className="circle-diagram-red">
+            <div className="circle-diagram-red-circle">
+              {[1, 2].map((item) => {
+                return (
+                  <div key={item} className="circle-diagram-red-rotate-wrapper">
+                    <CircleDiagramRedItem />
+                    <CircleDiagramRedItem />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
           <div className="circle-diagram-line-circle">
-            <div className="circle-diagram-icon-inner">
-              <CircleDiagramInnerItem />
-              <CircleDiagramInnerItem />
-            </div>
-
-            <div className="circle-diagram-icon-inner">
-              <CircleDiagramInnerItem />
-              <CircleDiagramInnerItem />
-            </div>
-
-            <div className="circle-diagram-icon-inner">
-              <CircleDiagramInnerItem />
-              <CircleDiagramInnerItem />
-            </div>
+            {[1, 2, 3].map((item) => {
+              return (
+                <div key={item} className="circle-diagram-icon-inner">
+                  <CircleDiagramInnerItem />
+                  <CircleDiagramInnerItem />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
