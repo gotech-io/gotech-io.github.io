a34b5f3d19c9d77e033ed829263d6f9906888560
Alina Khomenker <alina.khomenker@gotech.io>
2022-12-29 12:26:26 +0200

    icons

---
+++ assets/js/common/icons/index.js
@@ -0,0 +1,32 @@
+export const ICONS = {
+  WEB_DEV: "fa-solid fa-laptop-code",
+  MOBILE_DEV: "fa-solid fa-mobile-screen-button",
+  DEVOPS_DEV: "fa-solid fa-cloud-arrow-up",
+  QA_DEV: "fa-solid fa-magnifying-glass",
+
+  FRONT_DEV: "fa-solid fa-chalkboard-user",
+  BACK_DEV: "fa-solid fa-database",
+
+  CROSS_DEV: "fa-solid fa-check-double",
+  NATIVE_DEV: "fa-brands fa-app-store-ios",
+
+  QA_AUTO: "fa-solid fa-bug-slash",
+  QA_MANUAL: "fa-solid fa-thumbs-up",
+
+  AUTO_DEV: "fa-solid fa-infinity",
+  ENV_DEV: "fa-solid fa-user-gear",
+  EtE_TEST: "fa-solid fa-check-double",
+  INTEGRATION: "fa-solid fa-arrows-spin",
+
+  FACEBOOK: "fa-brands fa-square-facebook",
+  LINKEDIN: "fa-brands fa-linkedin",
+  INSTAGRAM: "fa-brands fa-square-instagram",
+
+  CHECK: "fa-brands fa-network-wired",
+  PIE: "fa-solid fa-chart-pie",
+  AWS: "fa-solid fa-aws",
+  DOCKER: "fa-solid fa-docker",
+  STACK: "fa-brands fa-stack-overflow",
+  SLACK: "fa-brands fa-slack",
+};
+<i class="fa-solid fa-list-check"></i>;
--- code/common/circleDiagram/index.js
+++ code/common/circleDiagram/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { ICONS } from "../../../assets/js/common/icons";
 
-const services = {
+const content = {
   title: "GoTech Group Provides",
   description:
     "State-of-the-art services, including highly qualified experts in all areas of infrastructure and development. Our services include Web Development, Mobile Development, DevOps, and QA Automation.",
+  services: [
+    { f: ICONS.WEB_DEV, s: ICONS.MOBILE_DEV },
+    { f: ICONS.DEVOPS_DEV, s: ICONS.QA_DEV },
+  ],
+  technologies: [
+    { f: ICONS.CHECK, s: ICONS.PIE },
+    { f: ICONS.AWS, s: ICONS.DOCKER },
+    { f: ICONS.SLACK, s: ICONS.STACK },
+  ],
 };
 
 function CircleDiagramServicesItem() {
@@ -28,8 +38,8 @@ function CircleDiagram() {
       <div className="circle-diagram">
         <div className="circle-diagram-content">
           <div>
-            <h4>{services.title}</h4>
-            <p>{services.description}</p>
+            <h4>{content.title}</h4>
+            <p>{content.description}</p>
           </div>
         </div>
 
