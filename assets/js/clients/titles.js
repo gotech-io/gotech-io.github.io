0bfb83ed5e7cfac9c71089b6d88681548f512c83
Alina Khomenker <alina.khomenker@gotech.io>
2023-01-02 12:35:20 +0200

    rename

--- assets/js/clients/titles.js
+++ assets/js/clients/titles.js
@@ -2,7 +2,7 @@ export const CLIENT_TITLES = {
   HEALTHCARE: "Healthcare",
   EDUCATION: "Education",
   SHOPPING: "Commerce & Retail",
-  CRYPTOCURRENCY: "Cryptocurrency",
+  CRYPTOCURRENCY: "Finance & Cryptocurrency",
   IT: "Cyber & IT",
   GOVERNMENT: "Government",
 };
