8da732fd985cbf3909852cd98e339e0cfaa93c8d
Alina Khomenker <alina.khomenker@gotech.io>
2023-01-03 14:11:31 +0200

    refactor

--- code/common/range/index.js
+++ code/common/range/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { ICONS } from "../../../assets/js/common/icons";
 
+const range = [1, 2, 3, 4, 5];
+
 function Range({ color, range }) {
   return (
     <div className="range-wrapper">
-      {[1, 2, 3, 4, 5].map((item) => {
-        const colorClass = item <= range ? `range-block-${color}` : "";
+      {range.length &&
+        range.map((item) => {
+          const colorClass = item <= range ? `range-block-${color}` : "";
 
-        return (
-          <div key={item} className="range-block">
-            <i className={`${ICONS.STAR} ${colorClass}`} />
-          </div>
-        );
-      })}
+          return (
+            <div key={item} className="range-block">
+              <i className={`${ICONS.STAR} ${colorClass}`} />
+            </div>
+          );
+        })}
     </div>
   );
 }
