52f412e1dc9e3e58464529e0114ba2b8931f456e
Alina Khomenker <alina.khomenker@gotech.io>
2023-01-05 14:18:01 +0200

    added a texts

--- assets/js/common/timeline/index.js
+++ assets/js/common/timeline/index.js
@@ -2,58 +2,57 @@ import { COLORS } from "../colors";
 
 export const TIMELINE = [
   {
-    title: "Proin sed libero",
+    title: "We are born!",
     description:
-      "Non enim praesent elementum facilisis leo vel. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus. Nunc non blandit massa enim nec. Pretium aenean pharetra magna ac placerat vestibulum lectus. Fermentum odio eu feugiat pretium nibh.",
-    year: 2016,
+      "We were founded in 2016 by 3 like-minded people whose priority is to maximize the level of technical services in Israel. We started with a small office and small clients. Having come a long way, now we understand all the stages that young startups go through every day, and we are ready to support and guide them.",
     color: COLORS.GREEN,
   },
   {
-    title: "Quis commodo odio aenean sed",
+    title: "First step into the Future",
     description:
-      "Porttitor lacus luctus accumsan tortor posuere ac. Urna molestie at elementum eu facilisis sed odio morbi quis. Pulvinar elementum integer enim neque volutpat ac tincidunt. Viverra adipiscing at in tellus integer feugiat.",
+      "We are worked so hard and by the beginning of 2017 we are moved to a new office and our team grow to 15 people. Our Company began to gain strength and break into the market to earn a reputation and a name for ourselves.",
     year: 2017,
     color: COLORS.BLUE,
   },
   {
-    title: "Commodo odio aenean",
+    title: "Major Achievements",
     description:
-      "Aliquet porttitor lacus luctus accumsan tortor posuere ac. Pellentesque dignissim enim sit amet venenatis urna cursus eget nunc. Tortor aliquam nulla facilisi cras fermentum. Justo nec ultrices dui sapien. In iaculis nunc sed augue lacus viverra vitae congue. Quam vulputate dignissim suspendisse in.",
+      "2018 was a very successful year! We continued to grow. Opened an additional service for QA software testing. Teams have become largest, projects more interesting. Our teams start to work side by side with major global companies like Cisco, as well them working on projects from the Israeli police and govermant.",
     year: 2018,
     color: COLORS.ORANGE,
   },
   {
-    title: "Netus et malesuada",
+    title: "CORONA is comme...",
     description:
-      "Nullam vehicula ipsum a arcu cursus vitae congue. Feugiat in fermentum posuere urna nec. Suscipit adipiscing bibendum est ultricies integer. Fames ac turpis egestas sed tempus.",
+      "No Company can evolve without overcoming difficult situations and is always dependent on the world. 2019 was remembered by all te begginning of the corona and the sacrifices we had to make. We tried to retain the teams as much as possible, them switched to work remotely, many clients were vulnerable and suspended projects. Yes, it was been a tough year!",
     year: 2019,
     color: COLORS.YELLOW,
   },
   {
-    title: "Viverra nibh cras",
+    title: "We can help rebuild our world",
     description:
-      "Quam vulputate dignissim suspendisse in. Facilisi cras fermentum odio eu feugiat pretium. Turpis egestas sed tempus urna et pharetra pharetra massa. Iaculis eu non diam phasellus vestibulum lorem sed risus ultricies. Mauris sit amet massa vitae tortor",
+      "When the world rebuilding, it is important not to miss the moment. In 2020, many buisnesses reaized that only on the web theu can survive the pandamic and began to look for good partners. We returned in full force and start to recruit even more enployees and projects. 2019 made us stronger and now we saw exactly where to move.",
     year: 2020,
     color: COLORS.PINK,
   },
   {
-    title: "In est ante in nibh",
+    title: "The New Heights",
     description:
-      "Tellus id interdum velit laoreet id donec ultrices. Aliquet porttitor lacus luctus accumsan tortor posuere ac. Pellentesque dignissim enim sit amet venenatis urna cursus eget nunc. Tortor aliquam nulla facilisi cras fermentum. Justo nec ultrices dui sapien.",
+      "In 2021 our company has more 100 employers. We have people from different countries such as Israel, America, Russia, Ukraine, Belarus, Uruguay. We have grown and it has become crowded in one office and one City. GoTech decided it was time to conquer the Tel-Aviv skyscrapers.",
     year: 2021,
     color: COLORS.PURPLE,
   },
   {
-    title: "Purus gravida quis",
+    title: "Getting closer to the Stars",
     description:
-      "Dolor sed viverra ipsum nunc aliquet bibendum enim facilisis. Eget mi proin sed libero enim sed. A arcu cursus vitae congue mauris rhoncus aenean vel. Dui nunc mattis enim ut tellus elementum sagittis vitae. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque.",
+      "In 2022, Our teams started work with such giants as Google, Walmart, Cognate, Varonis and many others. We help them to implements projects with cryptocurrency, artificial intelligence and QA Automations for various platforms.",
     year: 2022,
     color: COLORS.PRIME,
   },
   {
-    title: "Odio facilisis mauris",
+    title: "Our next targets",
     description:
-      "Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Vel pharetra vel turpis nunc eget lorem dolor sed. Purus in mollis nunc sed id semper. Ipsum dolor sit amet consectetur adipiscing elit. Quam viverra orci",
+      "Our plans for 2023 are to continue introducing exciting new technologies, working with incredible startups and expanding our professional team.",
     year: 2023,
     color: COLORS.ADDITIONAL,
   },
