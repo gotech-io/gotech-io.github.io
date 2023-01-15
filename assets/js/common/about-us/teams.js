4361d1909568466c8979e71a2cd48ccdef188855
Alina Khomenker <alina.khomenker@gotech.io>
2023-01-15 09:02:49 +0200

    about us refactor

--- assets/js/common/about-us/teams.js
+++ assets/js/common/about-us/teams.js
@@ -1,13 +1,7 @@
 export const TEAMS = [
   {
-    name: "Yaniv Peleg",
-    position: "CEO",
-    image: "/assets/images/home/team/yaniv.jpg",
-    linkedin: "https://www.linkedin.com/in/yaniv-peleg/",
-  },
-  {
     name: "Lidor Itzhari",
-    position: "COO",
+    position: "CEO",
     image: "/assets/images/home/team/lidor.jpg",
     linkedin: "https://www.linkedin.com/in/lidoritzhari/",
   },
@@ -34,12 +28,6 @@ export const TEAMS = [
     position: "Head of Mobile",
     image: "/assets/images/home/team/giora.jpg",
     linkedin: "https://www.linkedin.com/in/giora-krasilshchik-47ab8219/",
-  },
-  {
-    name: "Evgeny Tofler",
-    position: "Head of DBA",
-    image: "/assets/images/home/team/evgeny.jpg",
-    linkedin: "https://www.linkedin.com/in/evgeny-tofler-8221a15b/",
   },
   {
     name: "Mor Armoni", //Gur?
