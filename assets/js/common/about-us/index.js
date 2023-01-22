c524fec72c30d81007e2ea3a9ec8060d310cac94
Alina Khomenker <alina.khomenker@gotech.io>
2023-01-22 12:31:54 +0200

    about us page styles

---
+++ assets/js/common/about-us/index.js
@@ -0,0 +1,50 @@
+import { COLORS } from "../colors";
+
+export const ABOUT_US_DETAILS = {
+  title: "About Us",
+  description:
+    "We help Fortune 500 companies, startups, and enterprises to build relevant digital products, train their teams, solve business problems, and reduce costs while increasing efficiency.",
+
+  video: {
+    src: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:6880909146707787776?compact=1",
+    title: "GoTech",
+  },
+
+  sections: [
+    {
+      type: "section-alpha",
+      color: COLORS.DEFAULT,
+      title: "our history",
+      description:
+        "GoTech is a one-stop shop for infrastructures and development services. Our team consists of the best and most professional mobile and web developers, QA automation developers, DevOps, data engineers, and DBAs. We provide our clients with high-quality, reliable services that are tailored to their specific business needs.",
+    },
+    {
+      type: "section-beta",
+      color: COLORS.MAIN,
+      title: "you can see us here!",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      type: "section-gamma",
+      color: COLORS.GREEN,
+      title: "why choose us?",
+      description:
+        "With years of experience in the industry, our team has the skills and expertise to deliver top-notch solutions that drive real results for our clients. We are committed to providing exceptional customer service and working closely with our clients to ensure their success.",
+    },
+    {
+      type: "section-delta",
+      color: COLORS.MAIN,
+      title: "Meet our professional team!",
+      description:
+        "At GoTech, we believe in the power of collaboration and partnerships. We work closely with our clients to understand their unique challenges and develop solutions that are tailored to their specific needs. Our team is dedicated to helping our clients achieve their goals and succeed in today's competitive market.",
+    },
+    {
+      type: "section-epsilon",
+      color: COLORS.YELLOW,
+      title: "With us, you are a step ahead",
+      description:
+        "We don’t just simply initiate your project. We step into your shoes and perform a detailed analysis of your business landscape to give you the most purposeful solution. Contact us today to learn more about our services and see how we can help your business grow and thrive.",
+    },
+  ],
+};
---
+++ code/screens/about-us/components/timeline.js
@@ -0,0 +1,34 @@
+import React from "react";
+import TimelineItem from "./timelineItem";
+import VerticalLine from "./verticalLine";
+
+//import { TIMELINE } from "../../../../assets/js/common/timeline";
+
+const Timeline = () => {
+  return (
+    <div className="page-common-block">
+      <div className="timeline-wrapper">
+        <VerticalLine />
+
+        <div className="timeline-block">
+          {/* {TIMELINE.length &&
+            TIMELINE.map(({ title, description, year, color }, i) => {
+              return (
+                <TimelineItem
+                  key={title}
+                  title={title}
+                  description={description}
+                  number={i + 1}
+                  year={year}
+                  color={color}
+                  reverse={i % 2 === 0 ? "reverse" : ""}
+                />
+              );
+            })} */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;
