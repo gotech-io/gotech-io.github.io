f39f0ba72fafdeeef4b4d7d316de28517348a6a4
Alina Khomenker <alina.khomenker@gotech.io>
2023-01-30 09:22:17 +0200

    animation

--- code/screens/blogs/components/post-section.js
+++ code/screens/blogs/components/post-section.js
@@ -1,5 +1,6 @@
 import Post from "../../../common/post";
 import React from "react";
+import animations from "../../../../assets/js/animations";
 
 const PostSection = ({ posts }) => {
   return (
@@ -9,7 +10,12 @@ const PostSection = ({ posts }) => {
       {posts.length &&
         posts.map((post) => {
           return (
-            <a key={post.title} href={post.link}>
+            <a
+              key={post.title}
+              href={post.link}
+              data-aos-anchor-placement="top-center"
+              data-aos={animations.zoomIn}
+            >
               <Post post={post} />
             </a>
           );
